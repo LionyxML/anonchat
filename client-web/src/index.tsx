554420d741d37d8125f3e294694a8788b61b6c9f
LionyxML <rahul.juliato@gmail.com>
2023-06-17 12:00:48 -0300

    feat: client-web now defaults to dark theme

--- client-web/src/index.tsx
+++ client-web/src/index.tsx
@@ -1,13 +1,24 @@
 /* @refresh reload */
 import { render } from "solid-js/web";
 import App from "./App";
-import { CssBaseline } from "@suid/material";
+import { CssBaseline, ThemeProvider, createTheme } from "@suid/material";
+import { createSignal } from "solid-js";
+
+const [theme, setTheme] = createSignal(
+  createTheme({
+    palette: {
+      mode: "dark",
+    },
+  })
+);
 
 render(
   () => (
     <>
-      <CssBaseline sx={{ fontFamily: "Space Mono" }} />
-      <App />
+      <ThemeProvider theme={theme()}>
+        <CssBaseline sx={{ fontFamily: "Space Mono" }} />
+        <App />
+      </ThemeProvider>
     </>
   ),
   document.getElementById("root")!
